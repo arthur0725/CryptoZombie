5c8776300b8c699f516652fa642ffab4e281b645
arthur0725 <112574949+arthur0725@users.noreply.github.com>
2023-04-17 17:33:41 +0800

    this is just a test

---
+++ Web3.js
@@ -0,0 +1,71 @@
+// Here's how we would access our contract:
+/*
+const Web3 = require("web3");
+const web3 = new Web3("http://localhost:8545")*/
+
+if (window.ethereum) {
+  App.web3Provider = window.ethereum;
+  try {
+    // Request account access
+    await window.ethereum.enable();
+  } catch (error) {
+    // User denied account access...
+    console.error("User denied account access")
+  }
+}
+// Legacy dapp browsers...
+else if (window.web3) {
+  App.web3Provider = window.web3.currentProvider;
+}
+// If no injected web3 instance is detected, fall back to Ganache
+else {
+  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+}
+web3 = new Web3(App.web3Provider);
+
+var abi='[{"inputs":[{"internalType":"string","name":"_name","type":"string"}],"name":"createRandomZombie","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"zombies","outputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"uint256","name":"dna","type":"uint256"}],"stateMutability":"view","type":"function"}]'
+var ZombieFactoryContract = web3.eth.contract(abi)
+var contractAddress = "0x13e4DC21aA611e6bAF63a805D4C120dC7aFF29D2"/* our contract address on Ethereum after deploying */
+var ZombieFactory = ZombieFactoryContract.at(contractAddress)
+// `ZombieFactory` has access to our contract's public functions and events
+
+// some sort of event listener to take the text input:
+$("#ourButton").click(function(e) {
+  var name = $("#nameInput").val()
+  // Call our contract's `createRandomZombie` function:
+  ZombieFactory.createRandomZombie(name)
+})
+
+// Listen for the `NewZombie` event, and update the UI
+var event = ZombieFactory.NewZombie(function(error, result) {
+  if (error) return
+  generateZombie(result.zombieId, result.name, result.dna)
+})
+
+// take the Zombie dna, and update our image
+function generateZombie(id, name, dna) {
+  let dnaStr = String(dna)
+  // pad DNA with leading zeroes if it's less than 16 characters
+  while (dnaStr.length < 16)
+    dnaStr = "0" + dnaStr
+
+  let zombieDetails = {
+    // first 2 digits make up the head. We have 7 possible heads, so % 7
+    // to get a number 0 - 6, then add 1 to make it 1 - 7. Then we have 7
+    // image files named "head1.png" through "head7.png" we load based on
+    // this number:
+    headChoice: dnaStr.substring(0, 2) % 7 + 1,
+    // 2nd 2 digits make up the eyes, 11 variations:
+    eyeChoice: dnaStr.substring(2, 4) % 11 + 1,
+    // 6 variations of shirts:
+    shirtChoice: dnaStr.substring(4, 6) % 6 + 1,
+    // last 6 digits control color. Updated using CSS filter: hue-rotate
+    // which has 360 degrees:
+    skinColorChoice: parseInt(dnaStr.substring(6, 8) / 100 * 360),
+    eyeColorChoice: parseInt(dnaStr.substring(8, 10) / 100 * 360),
+    clothesColorChoice: parseInt(dnaStr.substring(10, 12) / 100 * 360),
+    zombieName: name,
+    zombieDescription: "A Level 1 CryptoZombie",
+  }
+  return zombieDetails
+}
